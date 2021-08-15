a9527ad70d46c4ad627306bb1f4cfd72a4a58302
Amada <Sigurd@Amadas-MacBook-Pro.local>
2021-08-15 15:21:50 +0800

    Added Frontend

--- server/express.js
+++ server/express.js
@@ -7,8 +7,11 @@ import helmet from 'helmet';
 import Template from './../template';
 import userRoutes from './routes/user.routes';
 import authRoutes from './routes/auth.routes';
+import devBundle from './devBundle'; // need to comment out this line for production
+import path from 'path';
 
 const app = express();
+const CURRENT_WORKING_DIR = process.cwd();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
@@ -25,11 +28,15 @@ app.get('/', (req, res) => {
 
 app.use((err, req, res, next) => {
     if (err.name === 'UnauthorizedError') {
-        res.status(401).jron({"error" : err.name + ": " + err.message})
+        res.status(401).json({"error" : err.name + ": " + err.message})
     } else if (err) {
         res.status(400).json({"error" : err.name + ": " + err.message})
         console.log(err)
     }
 })
+
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+
+devBundle.compile(app); // need to comment this out for production
 
 export default app;
