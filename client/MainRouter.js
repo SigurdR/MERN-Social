792c16b86994c4853a731a5e06f937bcfa07cdb7
Amada <Sigurd@Amadas-MacBook-Pro.local>
2021-08-16 23:12:58 +0800

    Completed Front End Functions

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -6,9 +6,14 @@ import Users from './user/Users';
 const MainRouter = () => {
     return (
         <div>
+            <Menu />
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route path="/users" component={Users}/>
+                <Route path="/signup" component={Signup}/>
+                <Route path="/signin" component={Signin}/>
+                <PrivateRoute path="/user/edit/:userId" component={EditProfile}/>
+                <Route path="/user/:userId" component={Profile}/>
             </Switch>
         </div>
     )
--- config/config.js
+++ config/config.js
@@ -1,8 +1,12 @@
+const MONGODB_USER = 'sa';
+const MONGODB_PASSWORD = 'melody039';
+const MONGODB_URI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@cluster0.32wdb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
 const config = {
     env: process.env.NODE_ENV || 'development',
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
-    mongoUri: process.env.MONGODB_URI || process.env.MONGO_HOST || 
+    mongoUri: MONGODB_URI || process.env.MONGO_HOST || 
     'mongodb://' + (process.env.IP || 'localhost') + ':' + 
     (process.env.MONGO_PORT || '27017') + 
     '/mernproject'
